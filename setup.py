7201b8aa9680dc0872f6c7f9b481b3a5b6cbdb44
Danny Shemesh <dany74q@gmail.com>
2016-02-21 17:55:11 +0200

    Added setup.py with dependencies

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='amazon_automation',
+      version='1.0',
+      description='Amazon Automation Porject',
+      url='http://github.com/netarachelhershko/amazon_automation',
+      author='Neta Hershko',
+      author_email='netarachelhershko@gmail.com',
+      license='MIT',
+      packages=[],
+      install_requires=[
+          'python-wordpress-xmlrpc',
+          'bitly_api',
+          'requests',
+          'BeautifulSoup',
+      ],
+      zip_safe=False)
